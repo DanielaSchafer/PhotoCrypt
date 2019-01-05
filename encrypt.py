07db2489e8aa869b4f63f3b56cd9dd880ea70b4f
Daniela Schafer <schaferd@winchesterthurston.org>
2019-01-05 14:33:44 -0500

    got pixels written

--- encrypt.py
+++ encrypt.py
@@ -8,18 +8,16 @@ p = re.compile("\\\\")
 password = b'hello'
 key = hashlib.sha256(password).digest()
 pass2 = Random.new() 
-print(key)
 
 iv = Random.new().read(AES.block_size)
 encryptor = AES.new(key, AES.MODE_CFB, iv)
 text = b'wow'
 msg = encryptor.encrypt(text)
 print(msg)
+print(iv)
 
 decryptor = AES.new(key, AES.MODE_CFB, iv)
 plain = decryptor.decrypt(msg)
-print(plain)
-print(iv)
 
 
 def remove_first_element(arr):
@@ -39,7 +37,6 @@ def remove_first_element(arr):
 #    for i in arr:
 
 
-
 def convert_to_ascii(arr):
     ascii_arr = list()
     for i in arr:
@@ -48,10 +45,11 @@ def convert_to_ascii(arr):
     return ascii_arr
 
 def split_into_arr(iv, msg):
-    iv_array = str(iv).split("'")[1].split("\\")
-    msg_array = str(msg).split("'")[1].split("\\")
+#    iv_array = str(iv).split("'")[1].split("\\")
+#    msg_array = str(msg).split("'")[1].split("\\")
 
-    iv_array = re.split(str(iv).split("'")[1])
+    iv_array = re.split(p,str(iv).split("'")[1])
+    msg_array = re.split(p,str(msg).split("'")[1])
     msg_array = remove_first_element(msg_array)
     iv_array = remove_first_element(iv_array)
     print(msg_array)
@@ -73,21 +71,24 @@ def get_ascii_arr(arr):
 def get_pixel_arr(arr):
     pixel_list = list()
     for i in arr:
-        for j in arr:
-            pixel_list.append((j/128)*255)
+        for counter, j in enumerate(i):
+            pixel_list.append(int((j/128)*255))
         pixel_list.append(0)
-
-
-
+    return pixel_list
 
 def write_ascii_file(arr, file_name):
+    get_pixel_arr(get_ascii_arr(arr))
+    iv_arr, msg_arr = split_into_arr(iv,msg)
     with open(file_path, "w+") as new_file:
         new_file.writelines(arr)
+
+def write_files()
+    iv_arr, msg_arr = split_int_arr(iv,msg)
+    write_ascii_file(iv_arr,"iv.csv")
+    write_ascii_file(msg_arr, "msg_arr")
+
         
 
-
-iv_arr, msg_arr = split_into_arr(iv,msg)
-get_ascii_arr(iv_arr)
 
 
 
